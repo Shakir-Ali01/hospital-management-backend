6840ef285e7a27975617aff3cc546eccb84a6fc0
shakir-Ali01 <connectwithshakirali@gmail.com>
2025-08-10 14:00:16 +0530

    Creted dto and entity for the user

---
+++ UserMS/src/main/java/com/hms/user/entity/User.java
@@ -0,0 +1,25 @@
+package com.hms.user.entity;
+
+import com.hms.user.dto.UserDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+    public UserDTO toDTO(){
+        return new UserDTO(this.id, this.name, this.email, this.password, this.role);
+    }
+}
